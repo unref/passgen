042b3f46b1327cb05578f8f6965864d8d1fa4b9a
pinepike <post@noks.ga>
2018-02-04 19:35:35 +0300

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var path = require('path');
+
+
+gulp.task('css', function() {
+  return gulp.src('src/**/*.css')
+    .pipe(autoprefixer({
+      browsers: ['last 5 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('js', function () {
+  return gulp.src('src/**/*.js')
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('img', function () {
+  return gulp.src('src/imgs/**/*.*')
+    .pipe(gulp.dest('dist/imgs'))
+})
+
+gulp.task('index', function () {
+  return gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.*', ['css', 'js', 'img', 'index'])
+});
+
+gulp.task('build', ['css', 'js', 'img', 'index']);
